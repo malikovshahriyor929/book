68ddf185474eb05192b68024dfa8b2a7d1883de0
Shaxriyor <shaxriyor@Shaxriyors-MacBook-Pro.local>
2026-01-20 15:06:17 +0500

    connecting db

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
-const exress = require('express');
-const { v4: uuidv4 } = require('uuid');
+const exress = require("express");
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+const postModel = require("./models/post.model");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = exress();
-app.get('/uuid', (req, res) => {
+
+app.use(exress.json());
+
+
+app.get("/uuid", (req, res) => {
   const newUuid = uuidv4();
   res.send({ uuid: newUuid });
 });
 
+app.post("/", async (req, res) => {
+  const { title, body } = req.body;
+  const newPost = await postModel.create({ title, body });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log(newPost);
+
+  res.json({ title, body });
 });
+
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  res.json({ id });
+});
+
+const Port = process.env.PORT || 3000;
+
+
+
+const starter = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL).then(() => console.log("Connected to DB"));
+    app.listen(Port, () => {
+      console.log("Server is running on port", Port);
+    });
+  } catch (error) {
+    console.log(`Connecting DB error: ${error}`);
+  }
+};
+
+starter();
