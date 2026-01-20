d41a41c9ce78981994f1a1446c0e83ca3d405e61
Shaxriyor <shaxriyor@Shaxriyors-MacBook-Pro.local>
2026-01-20 14:01:53 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const exress = require('express');
+const { v4: uuidv4 } = require('uuid');
+
+const app = exress();
+app.get('/uuid', (req, res) => {
+  const newUuid = uuidv4();
+  res.send({ uuid: newUuid });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
